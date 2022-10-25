f90d85e8bb7fe164664059a167380487243cc055
dkitStudent <dkitneeds@gmail.com>
2022-10-25 23:44:30 +0300

    Добавьте файлы проекта.

---
+++ ui-task-3-p-20-1-Pavlov/Core/ItemCount.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Media;
+
+namespace ui_task_p_20_1_Pavlov.Core
+{
+    public class ItemCount
+    {
+        public Brush Color { get; private set; }
+        public int Value { get; private set; }
+
+        public ItemCount(Brush color, int value)
+        {
+            Color = color;
+            Value = value;
+        }
+    }
+}
---
+++ ui-task-3-p-20-1-Pavlov/Core/MenuItem.cs
@@ -0,0 +1,25 @@
+﻿using MaterialDesignThemes.Wpf;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ui_task_p_20_1_Pavlov.Core
+{
+    public class MenuItem
+    {
+        public string Name { get; private set; }
+
+        public PackIconKind Icon { get; private set; }
+
+        public ItemCount Count { get; private set; }
+
+        public MenuItem(string name, PackIconKind icon, ItemCount count)
+        {
+            Name = name;
+            Icon = icon;
+            Count = count;
+        }
+    }
+}
